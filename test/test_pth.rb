640c1f193280312004dd1d12f19b432bf5f63cda
Paul Brannan <curlypaul924@gmail.com>
2008-11-21 13:58:20 -0500

    Added test script.

---
+++ test/test_pth.rb
@@ -0,0 +1,41 @@
+require 'test/unit'
+require 'pth'
+
+class TestPth < Test::Unit::TestCase
+  @@initialized = false
+
+  def setup
+    Pth.pth_init if not @@initialized
+    at_exit { Pth.pth_kill }
+    @@initialized = true
+  end
+
+  def test_pth_attr_new
+    attr = Pth.pth_attr_new
+  end
+
+  def test_spawn
+    attr = Pth.pth_attr_new
+    th = Pth.pth_spawn(attr, 0) { }
+  end
+
+  def test_join
+    attr = Pth.pth_attr_new
+    th = Pth.pth_spawn(attr, 0) { }
+    Pth.pth_join(th, 0)
+  end
+
+  def disabled__test_local_variable_set_in_thread
+    attr = Pth.pth_attr_new
+    run = false
+    th = Pth.pth_spawn(attr, 0) { run = true }
+    Pth.pth_join(th, 0)
+    assert run
+  end
+
+  def disabled__test_gc_in_thread
+    attr = Pth.pth_attr_new
+    th = Pth.pth_spawn(attr, 0) { GC.start }
+  end
+end
+
