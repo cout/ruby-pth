fa3d391f7d72236aa1e664ba51725b45cfbfb660
Paul Brannan <curlypaul924@gmail.com>
2008-12-05 17:37:32 -0500

    Added debugging output. Try setting to the other side of the stack to see if that helps -- it doesn't.

--- ext/pth_stack_fix/pth_stack_fix.c
+++ ext/pth_stack_fix/pth_stack_fix.c
@@ -6,7 +6,8 @@ extern VALUE *rb_gc_stack_start;
 
 static void on_pth_thread_switch(pth_t new_thread)
 {
-  rb_gc_stack_start = (VALUE *)new_thread->stack;
+  rb_gc_stack_start = (VALUE *)(new_thread->stack + new_thread->stacksize);
+  printf("setting stack start to %p\n", rb_gc_stack_start);
 }
 
 void Init_pth_stack_fix()
